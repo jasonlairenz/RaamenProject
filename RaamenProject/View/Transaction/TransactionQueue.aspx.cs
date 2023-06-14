7df785134ce45b6717e4c9dcd567452401693d1c
joshuajevon <joshuajevon05@gmail.com>
2023-06-14 16:00:19 +0700

    fix: navbar transaction queue and detal

--- RaamenProject/View/Transaction/TransactionQueue.aspx.cs
+++ RaamenProject/View/Transaction/TransactionQueue.aspx.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -38,6 +39,73 @@ namespace RaamenProject.View.Transaction
             int UserId = int.Parse(Request.QueryString["UserId"]);
             TransactionHeaderRepository.updateStatus(headerId);
             Response.Redirect("~/View/Transaction/TransactionQueue.aspx?UserId="+UserId);
+        }
+
+        public int getUserRole()
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            User u = UserController.viewUserById(id);
+            return u.Roleid;
+        }
+
+        protected void logoutBtn_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+
+            HttpCookie autkuki = new HttpCookie(FormsAuthentication.FormsCookieName, "");
+            autkuki.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(autkuki);
+
+            HttpCookie kuki = new HttpCookie("Nama Kuki", "");
+            kuki.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(kuki);
+
+            Response.Redirect("~/View/Authentication/Login.aspx");
+        }
+
+        protected void profileBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Profile/ViewProfile.aspx?UserId=" + id);
+        }
+
+        protected void orderRamenBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Ramen/orderRamen.aspx?UserId=" + id);
+        }
+
+        protected void orderQueueBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Transaction/TransactionQueue.aspx?UserId=" + id);
+        }
+
+        protected void manageRamenBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Ramen/viewRamen.aspx?UserId=" + id);
+        }
+
+        protected void historyBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Transaction/History.aspx?UserId=" + id);
+        }
+
+        protected void reportBtn_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Transaction/Report.aspx?UserId=" + id);
+        }
+
+
+
+        protected void homeBtn_Click1(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["UserId"]);
+            Response.Redirect("~/View/Home.aspx?UserId=" + id);
         }
 
     }
